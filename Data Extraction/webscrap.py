0889f04d692a441f5e491cd084c2322444d39b2a
harish gandhi ramachandran <hramachandran.haw.iit.edu>
2017-10-16 17:19:53 -0500

    used bs4 for beautifulsoup; implemented link extraction for 'statement' for every month

--- Data Extraction/webscrap.py
+++ Data Extraction/webscrap.py
@@ -1,30 +1,37 @@
 import urllib, os
-from BeautifulSoup import *
+# from BeautifulSoup import *
+from bs4 import BeautifulSoup
 import time
 import pprint
-
-url = 'https://www.federalreserve.gov/newsevents/pressreleases/monetary20160127a.htm'
-dir_path = "/home/harish/Desktop/test/"
-
-html = urllib.urlopen(url).read()
-soup = BeautifulSoup(html)
-# pprint.pprint(soup)
-# tags = soup.findAll('a', href=True)
-div_summary = soup.findAll('div', { "class" : "col-xs-12 col-sm-8 col-md-8" })
-# tags = div_summary.find_all('p')
-# print div_summary
-#
-# # paragraphs = soup.find_all('p')
-# # for paragraph in paragraphs:
-# #     print(paragraph.get_text(strip=True))
-# # # initialization
-# # saveas = []
-# # already_exist = bool
-
 import bleach
-html = "<b>Bad</b> <strong>Ugly</strong> <script>Evil()</script>"
-clean = bleach.clean(div_summary, tags=[], strip=True)
-print clean
+
+def eachStatement(url):
+    url = 'https://www.federalreserve.gov/newsevents/pressreleases/monetary20160127a.htm'
+    dir_path = "/home/harish/Desktop/test/"
+
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
+    # pprint.pprint(soup)
+    # tags = soup.findAll('a', href=True)
+    div_summary = soup.findAll('div', { "class" : "col-xs-12 col-sm-8 col-md-8" })
+    # html = "<b>Bad</b> <strong>Ugly</strong> <script>Evil()</script>"
+    clean = bleach.clean(div_summary, tags=[], strip=True)
+    print clean
+
+url_mainpage = 'https://www.federalreserve.gov/monetarypolicy/fomccalendars.htm'
+htmls = urllib.urlopen(url_mainpage).read()
+soups = BeautifulSoup(htmls)
+saveas = []
+already_exist = bool
+# tags = soups.findAll('a', href=True)
+# divs = soups.findAll('div',{ "class" : "col-xs-12 col-md-4 col-lg-2" })
+hrefstatement = soups.findAll('a', href=True, text='Statement')
+# print hrefstatement
+for div in hrefstatement:
+    print div['href']
+    # if div.find('a').contents[0] == 'Statement':
+    #     print div.find('a')
+
 
 # # Checking in the local folder for the filename
 # def check_for_file(fname):
@@ -49,13 +56,13 @@ print clean
 #     return True
 #
 #
-# # looping through links
+# looping through links
 # for link in tags:
 #     filename = link.get('href')
 #     # download_link = url + filename
 #     print filename
-
-    # if filename[len(filename) - 3:] == 'pdf':
-    #     saveas = filename
-    #     saveas_splited = saveas.split('/')
-    #     write_to_folder(saveas_splited)
+#
+#     if filename[len(filename) - 3:] == 'pdf':
+#         saveas = filename
+#         saveas_splited = saveas.split('/')
+#         write_to_folder(saveas_splited)
